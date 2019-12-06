225e30fe9b34c2cda80c4977f4f1c98fb2a6312e
unknown <egboh.catherine@utexas.edu>
2019-12-05 20:51:57 -0600

    updated to include parameter path

--- PyPoll/pypoll.py
+++ PyPoll/pypoll.py
@@ -1,22 +1,20 @@
 import csv
-import os
 
-path = r"C:\Users\Cathy\Desktop\UT_CourseWork\hw-2\python-challenge"
-filename = os.path.join(path, "election_data.csv")
-with open(filename, "r") as csv_data:
-    reader = csv.DictReader(csv_data, delimiter = ',')
-    count = 0
-    votes_list = []
-    for row in reader:
-        if count ==0:
-            pass
-            count +=1
-        votes_list.append(row["Candidate"])
-    total = len(list(votes_list))
-    print("Election Results\n-----------------")
-    print("Total Votes: %d\n------------------" %(total))
-    candidate_list = set(votes_list)
-    for i in candidate_list:
-        votes = votes_list.count(i)
-        percent_votes = (votes/total) * 100
-        print("%s: %.2f%% (%d)" %(i, percent_votes, votes))
+def election(filename):
+    with open(filename, "r") as csv_data:
+        reader = csv.DictReader(csv_data, delimiter = ',')
+        count = 0
+        votes_list = []
+        for row in reader:
+            if count == 0:
+                pass
+                count +=1
+            votes_list.append(row["Candidate"])
+        total = len(list(votes_list))
+        print("Election Results\n-----------------")
+        print("Total Votes: %d\n------------------" %(total))
+        candidate_list = set(votes_list)
+        for i in candidate_list:
+            votes = votes_list.count(i)
+            percent_votes = (votes/total) * 100
+            print("%s: %.2f%% (%d)" %(i, percent_votes, votes))
